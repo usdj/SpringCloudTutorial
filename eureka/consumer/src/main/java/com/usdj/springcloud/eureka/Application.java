7bb37d35daf5329fc2c7c2e104d6cdbf97b48219
jerry <denggerry@163.com>
2019-03-08 23:17:21 +0800

    Finish Ribbon
    It has 3 ways to realize

--- eureka/consumer/src/main/java/com/usdj/springcloud/eureka/Application.java
+++ eureka/consumer/src/main/java/com/usdj/springcloud/eureka/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -17,10 +18,18 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 public class Application {
 
-	@Bean
-	@LoadBalanced
+
+
+    @Bean
+    @LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Primary
+	@Bean
+	RestTemplate lbcRestTemplate() {
+    	return new RestTemplate();
 	}
 
 	public static void main(String[] args) {
